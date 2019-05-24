4cee6563ea67c3a6d6805f5b8fdca1422e8c9d40
cmclar <cmclar204@caledonian.ac.uk>
2019-05-24 15:43:13 +0100

    Completed the module

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -9,6 +9,6 @@ namespace StarChart.Data
         {
         }
 
-        public DbSet<CelestialObject> MyProperty { get; set; }
+        public DbSet<CelestialObject> CelestialObjects { get; set; }
     }
 }
--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -14,7 +14,7 @@ namespace StarChart.Models
         [Required]
         public string Name { get; set; }
 
-        public int? OrbitedobjectId { get; set; }
+        public int? OrbitedObjectId { get; set; }
 
         [NotMapped]
         public List<CelestialObject> Satellites { get; set; }
